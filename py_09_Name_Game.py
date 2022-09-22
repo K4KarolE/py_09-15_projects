17817a77c5f7a4c6cc4aa1b40936ddbe27a7f6be
K4KarolE <karoly.egyed@protonmail.com>
2022-09-22 17:24:21 +0100

    jambo

--- py_09_Name_Game.py
+++ py_09_Name_Game.py
@@ -2,38 +2,40 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 print('')
-print('Give me two english names and I will tranform them into one for you.')
+print('Give me an english couple`s first names and I will tranform them into baby names.')
 
 
-firstname = input('First Name: ')
+firstname = input('Husband`s First Name: ')
 firstname = firstname.lower()
 while True:
     for i in firstname:
         if i not in alphabet:
             print()
             print('Please add a proper english first name, no special characters, no numbers.')
-            firstname = input('First Name: ')
+            firstname = input('Husband`s First Name: ')
             break
     else:
         break
 
 
-secondname = input('Second Name: ')
+secondname = input('Wife`s First Name: ')
 secondname = secondname.lower()
 while True:
-    for i in secondname:
+    for i in secondname:         
         if i not in alphabet:
             print()
             print('Please add a proper english second name, no special characters, no numbers.')
-            secondname = input('Second Name: ')
+            secondname = input('Wife`s First Name: ')
             break
     else:
         break
 
 fn_value = 0
 for i in firstname:
-    n = alphabet.index(i) + 1
-    fn_value = fn_value + n
+    n = alphabet.index(i) + 1   #looking for the position of the first name`s letters in alphabet
+    fn_value = fn_value + n     #sum of the position/index of the letters
+
+# print(fn_value)
 
 
 sn_value = 0
@@ -41,10 +43,20 @@ for i in firstname:
     n = alphabet.index(i) + 1
     sn_value = sn_value + n
 
+# print(sn_value)
+
 
 first_letter_number = fn_value*sn_value
+
+# print(first_letter_number)
+
 first_letter_number = fn_value*sn_value%26
 
 
 first_letter = alphabet[first_letter_number-1]
+
+# print(first_letter_number)
+
+print()
+
 print(first_letter)
---
+++ py_10_Rock_Paper_Scissors.py
@@ -0,0 +1,16 @@
+
+
+win = 0
+loss = 0
+draw = 0
+
+print()
+print('ROCK PAPER SCISSORS')
+print('Win(' + str(win) + ') Loss(' + str(loss) + ') Draw(' + str(draw) + ')')
+
+
+
+
+
+
+
