298b952818cd9133189811f23e9afa36ac64d1f3
K4KarolE <karoly.egyed@protonmail.com>
2022-09-24 22:40:54 +0100

    ok

--- py_12_Collatz_sequence.py
+++ py_12_Collatz_sequence.py
@@ -1,31 +1,37 @@
 
-
 import sys
 
+def collatz():
+    while True:
+        number = input('Add a positive, int number and I will "Collatz collapse" it for you. ')
+        if number.isdigit():
+            number = int(number)
+            if number == 1:
+                print()
+                print('Come on bro, give me a bigger one. (That what she said.)')
+            else:
+                break
+        else:
+            print()
+            print('Sorry buddy, it has to be a positive number.')
+    return number
 
-number = input('Give me a number and I will "Collatz collapse" it for you. ')
 
-def collatz(number):
-    if number % 2 == 0:
-        number //=2
-        print(number)
-        return number 
-    elif number % 1 == 1:
-        number = number * 3 + 1
-        print(number)
-        return number
-
+print()
+dumber = collatz()
 try:
     while True:
-        if type(number) == int:
-            collatz(number)
+        if dumber % 2 == 0:
+            dumber //= 2
+            print(dumber)
         
-        elif type(number) != int:
-            number = input('Give me a number and I will "Collatz collapse" it for you. ')
+        elif dumber == 1:
+            break
+       
+        elif dumber % 2 == 1:
+            dumber = dumber * 3 + 1
+            print(dumber)
+            
         
 except KeyboardInterrupt:
     sys.exit()
-
-
-x = type(333)
-print(x)
