b7012acd9b0022a4b6e01075529b6ad75533e00a
K4KarolE <karoly.egyed@protonmail.com>
2022-09-26 19:49:41 +0100

    ready

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 - Python not able to handle: 'name(string)' * 'name(string)' so what if..
 - asking to input 2 english names
 - validating them, no numbers, no special characters
-- turn the names into numbers by allocating the letters from the name to it`s position in alphabet: a=1, b=2 // Peter = 16+5+20+5+18 
+- turn the names into numbers by allocating the letters from the name to it`s index in alphabet: a=1, b=2 // Peter = 16+5+20+5+18 
 - multiply the 2 numbers (from the names) with eachother
-- modulo 26 (as many the letters in the US aphabet)
+- result`s modulo 26 (26 letters in the US aphabet)
 - turn back that number into letter, that will be the first letter of the result/new name for both the boy and girl names
-- if there is more than 1 names with the same first letter`s group, choose randomly from this group for both the boy and girl names
+- create a list with name(s) with the first letter
+- if there is more than 1 items in this list: #choose randomly from this group for both the boy and girl names
 - if there is no name with the first letter in the list, that is sign of the evil and the couple should not produce that type of life till I am alive
 
 
--- py_09_Name_Game.py
+++ py_09_Name_Game.py
@@ -1,4 +1,13 @@
 
+
+import random
+from openpyxl import load_workbook
+wb = load_workbook('py_09_Name_Game-TOP-NAMES.xlsx', data_only=True)   #data_only=True -> copying values from excel instead of formulas for the Haveseen cell value
+ws = wb.active
+
+
+# Asking the names of the couples, generating the first letter of the new name of their baby
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 print('')
@@ -35,28 +44,55 @@ for i in firstname:
     n = alphabet.index(i) + 1   #looking for the position of the first name`s letters in alphabet
     fn_value = fn_value + n     #sum of the position/index of the letters
 
-# print(fn_value)
-
 
 sn_value = 0
 for i in firstname:
     n = alphabet.index(i) + 1
     sn_value = sn_value + n
 
-# print(sn_value)
 
 
 first_letter_number = fn_value*sn_value
 
-# print(first_letter_number)
+first_letter_number = fn_value*sn_value%26  # 26 letters in the US alphabet
 
-first_letter_number = fn_value*sn_value%26
+first_letter = alphabet[first_letter_number-1].upper()
 
 
-first_letter = alphabet[first_letter_number-1]
 
-# print(first_letter_number)
+# ----- Collecting the correct name(s) from Excel to a list, from the list back to the user -----
+
+
+boy_list = []   # lists where we collect all the names with the first letter generated from the couples names
+girl_list = []
+
+for cellnumber in range(5,105):
+    cell_boy = 'B' + str(cellnumber)
+    boyname = ws[cell_boy].value
+    if boyname[0] == first_letter:
+        boy_list = boy_list + [boyname]
+    
+
+for cellnumber in range(5,105):
+    cell_girl = 'F' + str(cellnumber)
+    girlname = ws[cell_girl].value
+    if girlname[0] == first_letter:
+        girl_list = girl_list + [girlname]
+
 
 print()
+print('If it is a')
 
-print(first_letter)
+
+if boy_list == []:
+    print('boy: 666   / for all the souls on the planet, pleae do not give a life for this "boy" /')
+else:
+    print('boy: ' + random.choice(boy_list))
+
+
+if girl_list == []:
+    print('girl: Backy  / Have you seen the Pet Sematary? /')
+else:
+    print('girl: ' + random.choice(girl_list))    
+
+print()
