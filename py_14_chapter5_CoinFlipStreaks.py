aadc1af55fdfce842ab75b42e4561c92d56452bf
K4KarolE <karoly.egyed@protonmail.com>
2022-09-29 17:23:16 +0100

    streak c

--- py_14_chapter5_CoinFlipStreaks.py
+++ py_14_chapter5_CoinFlipStreaks.py
@@ -2,21 +2,27 @@
 
 import random
 
-value_list = []
+
 max_range = 100
-counter = 0
-
-for i in range(0, max_range):
-    value = random.randint(0,1)
-    value_list.append(value)
+counterTT = 0
 
 
-for p in range(0, max_range-6):
-    if value_list[p] == value_list[p + 1] == value_list[p + 2] == value_list[p + 3] == value_list[p + 4] == value_list[p + 5] == value_list[p + 6]:
-        counter = 1
+for q in range(10000):
+    counterH = 0
+    value_list = []
+    for i in range(0, max_range):
+            value = random.randint(0,1)
+            value_list.append(value)
+
+    for p in range(max_range-6):
+        if value_list[p] == value_list[p + 1] == value_list[p + 2] == value_list[p + 3] == value_list[p + 4] == value_list[p + 5] == value_list[p + 6]:
+            counterTT +=1       
+        
+# what is a streak count? is it duplicate when the streak is more than 6 digit long?        
+
 
 
 print()
-print(value_list)
-print(counter)
+# print(value_list)
+print(counterTT)
 
